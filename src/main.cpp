f7ad644d01de2834d710ef23ffab68f629fd4794
陈天垚 <try.skycn@Gmail.com>
2015-07-09 20:42:45 +0800

    Create main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/*
+	File: main.cpp
+	Project: MinCut
+	Author: Tianyao Chen
+	Date: July 9, 2015
+*/
+
+#include "graph.h"
+#include <cstdio>
+
+int main() {
+
+	graph G;
+
+	int n, m;
+	scanf("%d%d", &n, &m);
+	G.initialize(n);
+
+	int x, y;
+	for (int i = 0; i < m; ++i) {
+		scanf("%d%d", &x, &y);
+		G.pushEdge(x, y);
+	}
+
+	double error;
+	scanf("%lf", &error);
+	printf("%d\n", G.calcMinCut(error));
+
+	return 0;
+}
